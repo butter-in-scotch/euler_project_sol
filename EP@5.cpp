c3921a976b0d1949f69e6ee748d58bebd876ffc0
butter-in-scotch <rahulsinghrathore0101@gmail.com>
2026-05-30 17:54:01 +0530

    Add files via upload

---
+++ EP@5.cpp
@@ -0,0 +1,8 @@
+//for a number to be divisible be both 2;', it must be even;
+//for a number to be divisible by both 2 and 3; it must be divisible by 2*3 = 6;
+//for a number to be divisible by both 2 and 4, it must be divisible by the lowest common multile of 2 and 4, thats 4;
+// for a number to be divisible by both 2 and 3, it must be divisible by their lowest common multiple, thats 2*3 = 6;
+// for a number to divisible by 1,2,3,4,5,6,7,8,9,10; it must be divisible by their lcm. that is ...2520
+// so lcm from 1 to 20 is .. 232792560
+
+
